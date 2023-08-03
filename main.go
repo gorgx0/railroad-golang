8d5af81d7a990f2df14f231b9aab0adc0680dc06
Marcin Krasowski <marcin.krasowski@gmail.com>
2023-08-03 03:16:47 +0200

    read stations and stores in db

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"railway/database"
 	"railway/model"
@@ -8,7 +9,16 @@ import (
 
 func main() {
 	stations := model.LoadStations("stations.json")
+
 	db := database.GetDBConnection()
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Panicf(err.Error())
+		}
+	}(db)
+
 	for _, station := range stations {
 		_, err := db.Exec("INSERT INTO stations (id, name, lat, lng) VALUES ($1, $2, $3, $4)", station.Id, station.Name, station.Lat, station.Lng)
 		if err != nil {
