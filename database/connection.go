b1c32289984af4736c216014a60041cd1e8f9381
Marcin Krasowski <marcin.krasowski@gmail.com>
2023-09-03 23:05:47 +0200

    Update connection.go

--- database/connection.go
+++ database/connection.go
@@ -10,6 +10,5 @@ import _ "github.com/lib/pq"
 func GetDBConnection(dbConfig config.DatabaseConfig) (*sql.DB, error) {
 	dbString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Password, dbConfig.Db)
 	postgres, err := sql.Open("postgres", dbString)
-	//postgres, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/railway?sslmode=disable")
 	return postgres, err
 }
