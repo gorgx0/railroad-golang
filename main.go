3e3331a3acd8422dd759c3332a16f58a2d4c844b
gorg <marcin.krasowski@gmail.com>
2023-09-04 02:43:23 +0200

    menu package created

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
-	"os"
 	"railway/config"
-	"railway/database"
-	"railway/model"
+	"railway/menu"
 )
 
 func main() {
@@ -17,109 +13,6 @@ func main() {
 	}
 
 	for {
-		menu(currentConfig)
-	}
-}
-
-func menu(currentConfig config.Config) bool {
-	fmt.Println("Menu:")
-	fmt.Println("1. Load stations from JSON file into database")
-	fmt.Println("2. Print all stations from database")
-	fmt.Println("3. Remove all stations from database")
-	fmt.Println("4. Exit")
-	fmt.Println("Enter your choice: ")
-
-	var choice string
-	var err error
-	_, err = fmt.Scanln(&choice)
-	if err != nil {
-		log.Panicf(err.Error())
-	}
-
-	switch choice {
-	case "1":
-		fmt.Println("Loading stations from JSON file into database")
-		loadingStationsFromJsonFile(currentConfig)
-	case "2":
-		fmt.Println("Printing all stations from database")
-		printAllStationsFromDatabase(currentConfig.Database)
-	case "3":
-		fmt.Println("Removing all stations from database")
-		removeAllStationFromDatabase(currentConfig.Database)
-	case "4":
-		fmt.Println("Exiting")
-		os.Exit(0)
-	default:
-		fmt.Println("Invalid choice")
-	}
-	return false
-}
-
-func removeAllStationFromDatabase(dbConfig config.DatabaseConfig) {
-	var db *sql.DB
-	var err error
-	db, err = database.GetDBConnection(dbConfig)
-	if err != nil {
-		log.Panicf(err.Error())
-	}
-
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			log.Panicf(err.Error())
-		}
-	}(db)
-
-	_, err = db.Exec("DELETE FROM stations")
-	if err != nil {
-		log.Panicf(err.Error())
-	}
-}
-
-func printAllStationsFromDatabase(dbConfig config.DatabaseConfig) {
-	var db *sql.DB
-	var err error
-	var stations []model.Station
-
-	db, err = database.GetDBConnection(dbConfig)
-
-	stations, err = model.GetAllStations(db)
-	if err != nil {
-		log.Panicf(err.Error())
-	}
-
-	fmt.Println("======== Stations Start ========")
-	for _, station := range stations {
-		station.Print()
-	}
-	fmt.Println("======== Stations End ========")
-}
-
-func loadingStationsFromJsonFile(config config.Config) {
-	var stations []model.Station
-	var err error
-	stations, err = model.LoadStationsFromJsonFile(config.StationsFile)
-	if err != nil {
-		log.Panicf(err.Error())
-	}
-
-	var db *sql.DB
-	db, err = database.GetDBConnection(config.Database)
-	if err != nil {
-		log.Panicf(err.Error())
-	}
-
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			log.Panicf(err.Error())
-		}
-	}(db)
-
-	for _, station := range stations {
-		err := station.Store(db)
-		if err != nil {
-			log.Println(err.Error())
-		}
+		menu.Menu(currentConfig)
 	}
 }
