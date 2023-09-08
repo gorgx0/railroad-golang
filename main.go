4fbe58c5d92c628e6c27137bc871f3fa1a53b70f
Marcin Krasowski <marcin.krasowski@gmail.com>
2023-09-08 02:55:27 +0200

    hw app

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 	"log"
 	"railway/config"
 	"railway/menu"
@@ -12,6 +14,10 @@ func main() {
 		log.Panicf(err.Error())
 	}
 
+	app := app.New()
+	w := app.NewWindow("Railway")
+	w.SetContent(widget.NewLabel("Hello Fyne!"))
+	w.ShowAndRun()
 	for {
 		err := menu.Menu(currentConfig)
 		if err != nil {
